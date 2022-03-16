a3af23aa3629f28ff3a54489f7ac44f56d9ab03d
Madan Chaudhary <madan712@gmail.com>
2022-03-16 15:14:07 +0000

    initial commit

--- bin/src/main/resources/schema.sql
+++
@@ -1,9 +0,0 @@
-use mydb;
-
-create table if not exists employee (
-empid varchar(10) not null,
-empname varchar(100) not null
-);
-
-insert into employee values ('101', 'foo');
-insert into employee values ('102', 'bar');
